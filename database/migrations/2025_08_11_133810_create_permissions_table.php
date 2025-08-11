04e0b5e8a0b2e139f30cc45fd5b8c4c3a07d7f85
MOULOUNGUI-Bienvenu <moulounguibienvenuesamara@gmail.com>
2025-08-11 17:15:14 +0200

    initialisation

---
+++ database/migrations/2025_08_11_133810_create_permissions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+             $table->foreignUuid('groupe_permision_id')->constrained()->onDelete('cascade');
+            $table->string('code')->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+    
+};
